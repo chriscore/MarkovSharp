d0d5ecd0aaf3a2eea9d362c22d2b4c077d411c63
chriscore <chriscore361@googlemail.com>
2017-05-25 22:41:27 +0100

    Adding MarkovSite example ASP net site

---
+++ MarkovSite/Controllers/HomeController.cs
@@ -0,0 +1,112 @@
+﻿using MarkovSharp.TokenisationStrategies;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarkovSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Get in touch";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Train()
+        {
+            var model = new StringMarkov(1);
+
+            string text;
+            using (Stream req = Request.InputStream)
+            {
+                req.Seek(0, SeekOrigin.Begin);
+                text = new StreamReader(req).ReadToEnd();
+            }
+
+            var lines = text.Split(new char[] { '\n', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            model.Learn(lines);
+
+            Session["Model"] = model;
+
+            return new JsonResult()
+            {
+                Data = $"Learnt {lines.Count()} lines of training data",
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        [HttpPost]
+        public ActionResult GetPredictions()
+        {
+            // this method is just a placeholder example
+
+            // TODO: use markovsharp nuget.
+            if (Session["Model"] != null)
+            {
+                var model = (StringMarkov)Session["Model"];
+
+                string text;
+                using (Stream req = Request.InputStream)
+                {
+                    req.Seek(0, SeekOrigin.Begin);
+                    text = new StreamReader(req).ReadToEnd();
+                }
+
+                if (string.IsNullOrEmpty(text))
+                {
+                    text = model.GetPrepadGram();
+                }
+
+                try
+                {
+                    var suggestions = model.GetMatches(text.Trim())
+                        .Where(a => 
+                            !string.IsNullOrWhiteSpace(a)
+                            && a != model.GetTerminatorGram()
+                        )
+                        .GroupBy(a => a)
+                        .OrderByDescending(a => a.Count())
+                        .Select(a => a.Key);
+
+                    return new JsonResult()
+                    {
+                        Data = suggestions,
+                        JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                    };
+                }
+                catch(Exception)
+                {
+                    return new JsonResult()
+                    {
+                        Data = new string[] { "-", "-", "-" }
+                    };
+                }
+            }
+            else
+            {
+                return new JsonResult()
+                {
+                    Data = new string[] { "-", "-", "-" }
+                };
+            }
+        }
+    }
+}
---
+++ MarkovSite/Scripts/Index.js
@@ -0,0 +1,74 @@
+﻿//setup before functions
+var typingTimer;               //timer identifier
+var doneTypingInterval = 200;  //time in ms, 5 second for example
+var $trainingArea = $('#training-input');
+var $input = $('#testing-input');
+
+function Train() {
+    console.log('training model');
+    var txt = $trainingArea.val();
+
+    $.post("/Home/Train", txt, function (data) {
+        console.log(data);
+        GetNextPredictions();
+    });
+}
+
+//on keyup, start the countdown
+$input.on('keyup', function () {
+    if ($input.is(':focus')){
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(GetNextPredictions, doneTypingInterval);
+    }
+    else{
+        console.log('not focused');
+    }
+});
+
+//on keydown, clear the countdown 
+$input.on('keydown', function () {
+    if ($input.is(':focus')) {
+        clearTimeout(typingTimer);
+    }
+    else {
+        console.log('not focused');
+    }
+});
+
+$("#suggestion-1").on('click', function () {
+    insertChoice($("#suggestion-1"));
+});
+
+$("#suggestion-2").on('click', function () {
+    insertChoice($("#suggestion-2"));
+});
+
+$("#suggestion-3").on('click', function () {
+    insertChoice($("#suggestion-3"));
+});
+
+function insertChoice($element) {
+    var currentText = $input.val();
+    if ($element.text() !== '-') {
+        var newText = currentText.concat(' ', $element.text());
+        console.log('Setting new text: '.concat(newText));
+        $input.val(newText);
+
+        GetNextPredictions();
+    }
+}
+
+//user is finished typing, get predictions
+function GetNextPredictions() {
+    console.log('getting predictions');
+    var txt = $input.val();
+
+    $.post("/Home/GetPredictions", txt, function (data)
+    {
+        console.log(data);
+
+        $("#suggestion-1").html(data[0] || '-');
+        $("#suggestion-2").html(data[1] || '-');
+        $("#suggestion-3").html(data[2] || '-');
+    });
+}
