ccac73907502104e26d150b19ad84e5b4e3a5f5a
Chris Core <chris.core@crispthinking.com>
2018-03-19 12:00:24 +0000

    Added UnigramSelector to generic model, override on implementation / in use to change behavior for state transitions.

---
+++ MarkovSharp/Components/UnweightedRandomUnigramSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovSharp.Components
+{
+    public class UnweightedRandomUnigramSelector<T> : IUnigramSelector<T>
+    {
+        public T SelectUnigram(IEnumerable<T> ngrams)
+        {
+            return ngrams.GroupBy(a => a)
+                .Select(a => a.FirstOrDefault())
+                .OrderBy(a => Guid.NewGuid())
+                .FirstOrDefault();
+        }
+    }
+}
---
+++ MarkovSharp/Components/WeightedRandomUnigramSelector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovSharp.Components
+{
+    public class WeightedRandomUnigramSelector<T> : IUnigramSelector<T>
+    {
+        public T SelectUnigram(IEnumerable<T> ngrams)
+        {
+            return ngrams.OrderBy(a => Guid.NewGuid()).FirstOrDefault();
+        }
+    }
+}
